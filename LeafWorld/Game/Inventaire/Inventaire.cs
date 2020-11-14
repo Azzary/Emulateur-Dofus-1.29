869d4513fb903bb32881e0371e86a4bf380309d8
Azzary <43534730+Azzary@users.noreply.github.com>
2020-11-14 19:37:25 +1100

    Add items in inv player

---
+++ LeafWorld/Game/Inventaire/Inventaire.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafWorld.Game.Inventaire
+{
+
+    public class Inventaire
+    {
+        public List<Item.Stuff> Stuff = new List<Item.Stuff>();
+
+
+    }
+}
---
+++ LeafWorld/Game/Item/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafWorld.Game.Item
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        
+        public int Niveau { get; set; }
+
+        public int Pods { get; set; }
+
+        public int Type { get; set; }
+
+        public string Stats { get; set; }
+        public string Condition { get; set; }
+        public string infosWeapon { get; set; }
+
+
+      
+        public Item(int _ID, int _pods, int _Niveau, int _Type, string _ModelStat, string _condition, string _infosWeapon)
+        {
+            ID = _ID;
+            Type = _Type;
+            Pods = _pods;
+            Niveau = _Niveau;
+            Stats = _ModelStat;
+            Condition = _condition;
+            infosWeapon = _infosWeapon;
+        }
+
+    }
+}
