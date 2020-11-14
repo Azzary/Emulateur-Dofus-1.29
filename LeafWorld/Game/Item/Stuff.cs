1868d82191abf1a60bdfde41250ce91ea50fbc18
Azzary <43534730+Azzary@users.noreply.github.com>
2020-11-14 19:37:37 +1100

    Create Stuff.cs

---
+++ LeafWorld/Game/Item/Stuff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafWorld.Game.Item
+{
+    public class Stuff : Item
+    {
+        public int Position { get; set; }
+        public readonly int UID;
+        public Stuff(int _ID, int _UID, int Niveau, int Type, int _position, string _Stats, string _condition, string _infosWeapon) : base(_ID, _UID, Niveau, Type, _Stats, _condition, _infosWeapon)
+        {
+            Position = _position;
+            UID = _UID;
+        }
+    }
+    
+}
